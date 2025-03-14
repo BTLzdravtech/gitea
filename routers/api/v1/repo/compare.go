7a17b927c52e3cb6bdfa3dcc8e6946c08ec23734
Tomas Hruska <hruskat@btlnet.com>
2025-03-14 23:22:08 +0100

    fix: compare API use head

--- routers/api/v1/repo/compare.go
+++ routers/api/v1/repo/compare.go
@@ -75,7 +75,7 @@ func CompareDiff(ctx *context.APIContext) {
 	apiCommits := make([]*api.Commit, 0, len(compareResult.compareInfo.Commits))
 	userCache := make(map[string]*user_model.User)
 	for i := 0; i < len(compareResult.compareInfo.Commits); i++ {
-		apiCommit, err := convert.ToCommit(ctx, ctx.Repo.Repository, ctx.Repo.GitRepo, compareResult.compareInfo.Commits[i], userCache,
+		apiCommit, err := convert.ToCommit(ctx, compareResult.headRepo, compareResult.headGitRepo, compareResult.compareInfo.Commits[i], userCache,
 			convert.ToCommitOptions{
 				Stat:         true,
 				Verification: verification,
